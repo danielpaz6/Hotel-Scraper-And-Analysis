36beb71135c532d80380ad3b11cddddd019e519f
Daniel Paz <danielpaz601@gmail.com>
2019-11-01 19:23:16 +0200

    Added p, d and q loops and their outputs to find the best parameters.

--- venv/arima_testing/analyzeHotel.py
+++ venv/arima_testing/analyzeHotel.py
@@ -12,6 +12,7 @@ from statsmodels.tsa.arima_model import ARIMA
 import statsmodels.api as sm
 import numpy as np
 from pandas import DataFrame
+from sklearn.metrics import mean_squared_error
 
 
 def parser(x):
@@ -28,30 +29,66 @@ size = int(len(X) * 0.66)
 train, test = X[0:size], X[size:len(X)]
 #train_dates, test_dates = series.index[0: size], series.index[size:len(X)]
 
-history = [x for x in train]
-predictions = list()
+#predictions = list()
 
 #df = pd.DataFrame({"val": pd.Series(fixedNan, index=series.index)})
 #df_train = pd.DataFrame({"val": pd.Series(train, index=train_dates)})
 #df_test = pd.DataFrame({"val": pd.Series(test, index=test_dates)})
 
+minError = 999999
+tmpn1 = -1
+tmpn2 = -1
+tmpn3 = -1
 
-for t in range(len(test)):
-	model = ARIMA(history, order=(5,1,0))
-	model_fit = model.fit(disp=0)
-	output = model_fit.forecast()
-	yhat = output[0]
-	predictions.append(yhat)
-	obs = test[t]
-	history.append(obs)
-	print('predicted=%f, expected=%f' % (yhat, obs))
+print("Running...")
+for n1 in range (1, 5):
+    for n2 in range(0, 5):
+        for n3 in range(0, 3):
+            if (n1,n2,n3) in [(1,0,0), (1,0,1), (1,0,2), (1,0,3), (1,0,4), (1,1,0), (1,1,1), (1,1,2), (1,2,0)]:
+                continue;
 
-error = mean_squared_error(test, predictions)
-print('Test MSE: %.3f' % error)
-# plot
-pyplot.plot(test)
-pyplot.plot(predictions, color='red')
-pyplot.show()
+            history = [x for x in train]
+            predictions = list()
+            for t in range(len(test)):
+                tmpOrder = (n1,n2,n3)
+                model = ARIMA(history, order=tmpOrder)
 
+                try:
+                    model_fit = model.fit(disp=0)
+                except:
+                    continue;
+
+                output = model_fit.forecast()
+                yhat = output[0]
+                predictions.append(yhat)
+                obs = test[t]
+                history.append(obs)
+                #print('predicted=%f, expected=%f' % (yhat, obs))
+
+            error = mean_squared_error(test, predictions)
+
+            if(error < minError):
+                minError = error
+                tmpn1 = n1
+                tmpn2 = n2
+                tmpn3 = n3
+
+            print("-----------------------------")
+            print('Test MSE: %.3f' % error)
+            print('p =  ' + n1.__str__())
+            print('d =  ' + n2.__str__())
+            print('q =  ' + n3.__str__())
+            print("-----------------------------")
+
+            # plot
+            #pyplot.plot(test)
+            #pyplot.plot(predictions, color='red')
+            #pyplot.show()
+
+
+print("MinError: " + minError.__str__())
+print("p: " + tmpn1.__str__())
+print("d: " + tmpn2.__str__())
+print("q: " + tmpn3.__str__())
 
 print("Done!")
